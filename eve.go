3e334c4afec7731c69b95eccc9c39224030d4a42
Daniel Melani <daniel.melani@gmail.com>
2013-08-27 17:17:41 +0200

    Starting to structure this stuff.

--- eve.go
+++ eve.go
@@ -1,9 +1,12 @@
-package main
+package eve
 
 import (
 	"encoding/xml"
 	"fmt"
+	"net/http"
+	"io/ioutil"
 )
+const eveUrl string = "https://api.eveonline.com"
 
 type Character struct {
 	Name	string	`xml:"name,attr"`
@@ -19,31 +22,40 @@ type CharactersResult struct {
 	CachedUntil	string		`xml:"cachedUntil"`
 }
 
-func main() {
-
+// Characters fetches, parses and returns a set of Eve Online characters.
+func Characters(keyID string, vCode string) (res CharactersResult) {
 	var v CharactersResult
-	data := `
-		<?xml version='1.0' encoding='UTF-8'?>
-		<eveapi version="2">
-		  <currentTime>2013-08-27 12:43:47</currentTime>
-		  <result>
-		    <rowset name="characters" key="characterID" columns="name,characterID,corporationName,corporationID">
-		      <row name="Daniel Derpington" characterID="93773652" corporationName="Science and Trade Institute" corporationID="1000045" />
-		    </rowset>
-		  </result>
-		  <cachedUntil>2013-08-27 13:40:34</cachedUntil>
-		</eveapi>
-		`
-	err := xml.Unmarshal([]byte(data), &v)
+	var err error
+	var data []byte
+
+	url := fmt.Sprintf("%s/account/Characters.xml.aspx?keyID=%s&%s", eveUrl, keyID, vCode)
+	data, err = fetch(url)
 	if err != nil {
-		fmt.Printf("error: %v", err)
+		fmt.Printf("Fetch error: %v", err)
 		return
 	}
-	fmt.Printf("XMLName: %#v\n", v.XMLName)
-	fmt.Printf("CurrentTime: %q\n", v.CurrentTime)
-	fmt.Printf("CachedUntil: %q\n", v.CachedUntil)
-	fmt.Printf("Name: %q\n", v.Characters[0].Name)
-	fmt.Printf("ID: %q\n", v.Characters[0].ID)
-	fmt.Printf("Corporation: %q\n", v.Characters[0].Corporation)
-	fmt.Printf("CorporationID: %q\n", v.Characters[0].CorporationID)
+
+	err = xml.Unmarshal([]byte(data), &v)
+	if err != nil {
+		fmt.Printf("Unmarshal error: %v", err)
+		return
+	}
+
+	return v
+}
+
+func fetch(url string) ([]byte, error) {
+	var err error
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	if err != nil {
+		return nil, err
+	}
+	return []byte(data), nil
 }
